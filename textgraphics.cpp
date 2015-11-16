392db8c601d4d957c0c7a6519ec34dc937c607b5
Martin <martin.favre22@gmail.com>
2015-11-16 22:21:35 +0100

    BashScriptftw

--- textgraphics.cpp
+++ textgraphics.cpp
@@ -7,31 +7,38 @@ class Textgrafs{
 public:
   Textgrafs();
   ~Textgrafs();
-  void print_row(std::string, int);
-  void print_col(std::string, int);
+  void add_row(std::string, int);
+  void add_col(std::string, int);
+  void print();
 private:
   int rows_;
   int cols_;
   char** grid;
 };
 
-  Textgrafs::Textgrafs(){
-    struct winsize w;
-    ioctl(0, TIOCGWINSZ, &w);
-    rows_ = w.ws_row;
-    cols_ = w.ws_col;
-    grid = new char*[rows_]; 
-    for(int i = 0; i < rows_; ++i){
-      grid[i] = new char[cols_];
-    }
+Textgrafs::Textgrafs(){
+  struct winsize w;
+  ioctl(0, TIOCGWINSZ, &w);
+  rows_ = w.ws_row;
+  cols_ = w.ws_col;
+  grid = new char*[cols_]; 
+  for(int i = 0; i < cols_; ++i){
+    grid[i] = new char[rows_];
   }
+}
 Textgrafs::~Textgrafs(){
   for(int i = 0; i < rows_; ++i){
     delete[] grid[i];
   }
   delete[] grid;
 }
-
-int main(){
-  Textgrafs p();
+void Textgrafs::print(){
+  for(int i = 0; i < cols_ ; ++i){
+    std::cout << grid[i] << std::endl;
+  }
 }
+int main(){
+  Textgrafs p;
+  p.print();
+}
+
