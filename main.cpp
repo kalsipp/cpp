b7d1aaedf05f471f888a4c92026e83a74f403498
Martin <martin.favre22@gmail.com>
2015-11-22 20:38:57 +0100

    Adding image to ascii conversion

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(){
   Img_container i("img_output");
   while(1){
     p.clear_grid();
-    p.add_border('e', 0, 0, p.cols_, p.rows_);
-    p.add_border('e', 2, 2, p.cols_-4, p.rows_-4);
-    p.add_row("A god among men.", 54, p.rows_-2);
+    p.add_border('#', 0, 0, p.cols_, p.rows_);
+    p.add_border('#', 2, 2, p.cols_-4, p.rows_-4);
+    p.add_row("Billions upon billions.", 54, p.rows_-2);
     //p.add_col("huehue", 20, 20); 
     //p.add_rect_unique(i.get_img_text(), 0, 0);
     p.print();
